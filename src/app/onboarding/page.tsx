eaa815ac1045d7ee0c8d59fe8c2bd9e76dc9e12c
Dramcatcherst <atrapasuenoshotel@gmail.com>
2026-09-23 13:42:36 -0600

    ux: pass safe first-value targets into onboarding

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -78,6 +78,7 @@ export default async function OnboardingPage() {
       firstName={view.firstName}
       businessName={view.businessName}
       journey={view.journey}
+      firstValueTargets={view.firstValueTargets}
     />
   );
 }
